07d3e2a1974b34ec38340044cc412ec3e26ef0a7
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-08-16 23:51:58 +0200

    fixed config and adeed sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,13 @@
 import preprocess from 'svelte-preprocess';
 
-export default preprocess();
+export default {
+	preprocess: preprocess({
+		defaults: {
+			script: 'typescript',
+			style: 'scss'
+		},
+		scss: {
+			includePaths: ['src/styles', 'node_modules']
+		}
+	})
+};
