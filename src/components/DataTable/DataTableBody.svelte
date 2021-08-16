6c7c3ac897e48d1c80c81c9da68bbf93ee809b4f
Abd El Rahaman Shehata <abdo.shehata1504@gmail.com>
2021-08-16 15:43:30 +0200

    added new props

--- src/components/DataTable/DataTableBody.svelte
+++ src/components/DataTable/DataTableBody.svelte
@@ -1,11 +1,13 @@
-<script>
-  let klass = '';
-  export { klass as class };
+<script lang="ts">
+	let klass = '';
+	export { klass as class };
+	export let style: string | undefined;
+	export let offsetHeight: number;
 </script>
+
+<tbody bind:offsetHeight {style} class="s-tbl-body {klass}">
+	<slot />
+</tbody>
 
 <style lang="scss" src="./DataTableBody.scss" global>
 </style>
-
-<tbody class="s-tbl-body {klass}">
-  <slot />
-</tbody>
